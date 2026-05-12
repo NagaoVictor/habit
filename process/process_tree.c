926c7d82b5efe385cb89b153a79225dccf56b87b
Nagao Victor <nagaovictor@hotmail.com>
2026-05-12 19:20:47 +0900

    add processes file

--- process/process_tree.c
+++ process/process_tree.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
 int main(){
   pid_t pid = fork();
@@ -9,12 +12,14 @@ int main(){
     printf("PID=%d PPID=%d\n",getpid(), getppid());
     fork();
     if(pid == 0){
-        printf("PID=%d PPID=%d\n",getpid(),getppid());
+        printf("PID=%d PPID=%d\n", getpid(), getppid());
     } else {
+         fork();
         printf("PID=%d PPID=%d\n",getpid(),getppid());
     }
   }
   else{
     printf("PID=%d PPID=%d\n", getpid(), getppid());
-  }
+    wait(NULL);
+   }
 }
